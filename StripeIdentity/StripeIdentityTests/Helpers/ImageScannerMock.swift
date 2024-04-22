7a8d9bb37b9e85649c39ae9d06dc8d7b319239d1
John Woo <wooj@stripe.com>
2024-04-22 11:42:39 -0700

    Stripe SDK 23.27.1

---
+++ StripeIdentity/StripeIdentityTests/Helpers/ImageScannerMock.swift
@@ -0,0 +1,49 @@
+//
+//  ImageScannerMock.swift
+//  StripeIdentityTests
+//
+//  Created by Mel Ludowise on 5/11/22.
+//  Copyright © 2022 Stripe, Inc. All rights reserved.
+//
+
+import CaptureCore
+import CoreVideo
+import Foundation
+@_spi(STP) import StripeCameraCore
+@_spi(STP) import StripeCore
+import XCTest
+
+@testable import StripeIdentity
+
+final class ImageScannerMock<Output>: ImageScanner {
+    var mlModelMetricsTrackers: [MLDetectorMetricsTrackerProtocol] = []
+
+    var scanResult: Result<Output, Error>
+
+    private(set) var didReset = false
+
+    init(
+        scanResult: Result<Output, Error> = .failure(NSError(domain: "", code: 0))
+    ) {
+        self.scanResult = scanResult
+    }
+
+    func scanImage(
+        pixelBuffer: CVPixelBuffer,
+        sampleBuffer: CMSampleBuffer,
+        cameraProperties: StripeCameraCore.CameraSession.DeviceProperties?
+    ) -> StripeCore.Future<Output> {
+        do {
+            return Promise(value: try scanResult.get())
+        } catch {
+            return Promise(error: error)
+        }
+    }
+
+    func reset() {
+        didReset = true
+    }
+}
+
+typealias DocumentScannerMock = ImageScannerMock<DocumentScannerOutput?>
+typealias FaceScannerMock = ImageScannerMock<FaceScannerOutput>
